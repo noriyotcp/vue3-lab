50675cc44ff5e9679081ca3d9fde531699bf54ca
noriyotcp <noriyo.akita@gmail.com>
2022-10-16 16:25:39 +0900

    Display item via v-for with :key

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <script setup>
 import { ref } from "vue";
-const item = ref({
-  id: 1,
-  name: "アボカドディップバケット",
-  description:
-    "刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。",
-  price: 480,
-  image: "/images/item1.jpg",
-});
-const tasks = ref(["タスクA", "タスクB", "タスクC"]);
+const items = ref([
+  {
+    id: 1,
+    name: "アボカドディップバケット",
+    description:
+      "刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。",
+    price: 480,
+    image: "/images/item1.jpg",
+  },
+  {
+    id: 2,
+    name: "あの日夢見たホットケーキ",
+    description:
+      "子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。",
+    price: 1180,
+    image: "/images/item2.jpg",
+  },
+  {
+    id: 3,
+    name: "HOP WTR",
+    description:
+      "ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。",
+    price: 320,
+    image: "/images/item3.jpg",
+  },
+  {
+    id: 4,
+    name: "チーズフレンチフライ",
+    description:
+      "イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。",
+    price: 670,
+    image: "/images/item4.jpg",
+  },
+]);
 </script>
 
 <template>
@@ -17,23 +42,20 @@ const tasks = ref(["タスクA", "タスクB", "タスクC"]);
     <h1>Vue.js ハンズオン</h1>
   </header>
   <main class="main">
-    <div class="item">
-      <div class="thumbnail">
-        <img :src="item.image" alt="" />
+    <template v-for="item in items" :key="item.id">
+      <div class="item">
+        <div class="thumbnail">
+          <img :src="item.image" alt="" />
+        </div>
+        <div class="description">
+          <h2>{{ item.name }}</h2>
+          <p>{{ item.description }}</p>
+          <span
+            >¥<span class="price">{{ item.price }}</span></span
+          >
+        </div>
       </div>
-      <div class="description">
-        <h2>{{ item.name }}</h2>
-        <p>{{ item.description }}</p>
-        <span
-          >¥<span class="price">{{ item.price }}</span></span
-        >
-      </div>
-    </div>
-    <div id="app">
-      <ul>
-        <li v-for="task in tasks">{{ task }}</li>
-      </ul>
-    </div>
+    </template>
   </main>
 </template>
 
