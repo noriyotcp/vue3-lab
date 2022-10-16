e8b167ba75a8be993b0f32231cd4cdd9cf2265cf
noriyotcp <noriyo.akita@gmail.com>
2022-10-16 16:22:09 +0900

    Display tasks via v-for

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,38 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 const item = ref({
-    id: 1,
-    name: 'アボカドディップバケット',
-    description:
-      '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
-    price: 480,
-    image: '/images/item1.jpg'
-})
+  id: 1,
+  name: "アボカドディップバケット",
+  description:
+    "刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。",
+  price: 480,
+  image: "/images/item1.jpg",
+});
+const tasks = ref(["タスクA", "タスクB", "タスクC"]);
 </script>
 
 <template>
   <header class="header">
-    <img
-      src="/images/logo.svg"
-      alt="">
+    <img src="/images/logo.svg" alt="" />
     <h1>Vue.js ハンズオン</h1>
   </header>
   <main class="main">
     <div class="item">
       <div class="thumbnail">
-        <img
-          :src="item.image"
-          alt="">
+        <img :src="item.image" alt="" />
       </div>
       <div class="description">
         <h2>{{ item.name }}</h2>
         <p>{{ item.description }}</p>
-        <span>¥<span class="price">{{ item.price }}</span></span>
+        <span
+          >¥<span class="price">{{ item.price }}</span></span
+        >
       </div>
+    </div>
+    <div id="app">
+      <ul>
+        <li v-for="task in tasks">{{ task }}</li>
+      </ul>
     </div>
   </main>
 </template>
